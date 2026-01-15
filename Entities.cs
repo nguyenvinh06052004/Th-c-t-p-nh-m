f9f6f87f87baf696c6ba013335f22e86bddee5d7
hoangmy17 <hoangmy1732004@gmail.com>
2026-01-15 19:30:29 +0700

    DataLayer

---
+++ Entities.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using DataLayer;
+
+public partial class Entities : DbContext
+{
+    private Entities(DbConnection connection, bool contextOwnsConnection = true)
+        : base(new EntityConnection(connection.ConnectionString), contextOwnsConnection)
+    { }
+    // Mỹ sửa property bảng từ object sang DbSet<CongTy>
+    public DbSet<CongTy> CongTy { get; set; }
+    public DbSet<DonVi> DonVi { get; set; }
+
+    public DbSet<KhachHang> KhachHang { get; set; }
+    public DbSet<SanPham> SanPham { get; set; }
+    public DbSet<Phong>   Phong { get; set; }
+    public DbSet<LoaiPhong> LoaiPhong { get; set; }
+    public DbSet<ThietBi> ThietBi { get; set; }
+    public DbSet<Phong_ThietBi> Phong_ThietBi { get; set; }
+    public DbSet<Tang> Tang { get; set; }
+    public DbSet<DatPhong> DatPhong { get; set; }
+
+    public DbSet<DatPhong_SanPham> DatPhong_SanPham { get; set; }
+
+    public DbSet<Param> Param { get; set; }
+
+    public DbSet<DatPhong_ChiTiet> DatPhong_ChiTiet { get; set; }
+
+    public static Entities CreateEntities(bool contextOwnsConnection = true)
+    {
+        // CHUỖI KẾT NỐI TỰ ĐIỀN VÀO ĐÂY
+        string servername = @"ADMIN-PC\SQLEXPRESS";
+        string database = "QUAN LI KHACH SAN";
+
+        // Build SqlConnectionString
+        SqlConnectionStringBuilder sql = new SqlConnectionStringBuilder
+        {
+            DataSource = servername,
+            InitialCatalog = database,
+            IntegratedSecurity = true,   // ← QUAN TRỌNG: Cho phép Windows Authentication
+            MultipleActiveResultSets = true
+        };
+
+
+        // Build EntityConnectionString
+        EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder
+        {
+            Provider = "System.Data.SqlClient",
+            ProviderConnectionString = sql.ConnectionString,
+            Metadata = @"res://*/KHACHSAN.csdl|res://*/KHACHSAN.ssdl|res://*/KHACHSAN.msl"
+        };
+
+        EntityConnection connection = new EntityConnection(entityBuilder.ConnectionString);
+        return new Entities(connection);
+    }
+}
---
+++ connect.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace DataLayer
+{
+    [Serializable]
+    public class connect
+    {
+        public string servername;
+        public string Servername
+        {
+            get { return servername; }
+            set { servername = value; }
+        }
+        
+        public string database;
+        public string Database
+        {
+            get { return database; }
+            set { database = value; }
+        }
+        public connect(string _servername,  string _database)
+        {
+
+            this.servername = _servername;
+            this.database = _database;
+        }
+        public void ConnectData()
+        {
+            if (File.Exists("connectdb.dba"))
+                File.Delete("connectdb.dba");
+            FileStream fs = File.Open("connectdb.dba", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Close();
+        }
+        public connect() { }
+    }
+}
